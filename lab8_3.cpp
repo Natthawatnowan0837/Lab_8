51708360c04fe201aea2939144298a48143218a6
JOnt <natthawatnowan@gmail.com>
2022-12-20 16:41:13 +0700

    Lab_8_3 complete

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,19 +1,17 @@
-#include<iostream>
-using namespace std;
+//ไม่ต้องเติม function main()
 
 char before(char x){
-	//Write your function definition here
-}
-
-int main(){
-	//Test Case
-	cout << before('A') << "\n";
-	cout << before('B') << "\n";
-	cout << before('P') << "\n";
-	cout << before('T') << "\n";
-	cout << before('Z') << "\n";
-	cout << before('a') << "\n";
-	cout << before('0') << "\n";
-	cout << before('c') << "\n";
-	return 0;
+    char result ;
+    if(x == 'A'){
+        result = 'Z';
+        return result;
+    }if(int(x)<65 || int(x)>90){
+        result = '0' ;
+        return result ;
+    }else{
+        result = x-1;
+        return result;
+    }
+    
+    
 }
